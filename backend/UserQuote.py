cfd75144bc461e578741a95cfcdffd7bd0c793d1
gordonch <gordonch@students.cs.ubc.ca>
2022-11-06 14:08:20 -0800

    added main and edited UserQuote

--- backend/UserQuote.py
+++ backend/UserQuote.py
@@ -5,19 +5,5 @@ class UserQuote():
         self.quote = quote
         self.likes = 0
 
-    def like(self, id):
-        if self.id == id:
-            self.likes += 1
-
-    def filter(self):
-       pass
-
-    # test
-    def results(self):
-        print(self.id)
-        print(self.username)
-        print(self.quote)
-        print(self.likes)
-
-test = UserQuote(1, "bob", "What is love")
-test.results()
+    def like(self):
+        self.likes += 1
---
+++ backend/main.py
@@ -0,0 +1,5 @@
+import UserQuote
+
+user1 = UserQuote(1, "gordon", "abc")
+
+user1.like()
