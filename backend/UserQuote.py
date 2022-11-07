adc010b73273e99589ec31fc3d888c0502729c48
gordonch <gordonch@students.cs.ubc.ca>
2022-11-06 17:22:01 -0800

    profanity check

--- backend/UserQuote.py
+++ backend/UserQuote.py
@@ -1,17 +1,20 @@
+from datetime import datetime
+
 class UserQuote():
     def __init__(self, id, username, quote):
         self.id = id
         self.username = username
         self.quote = quote
         self.likes = 0
+        self.time = datetime.now().timestamp()
 
     def like(self):
         self.likes += 1
 
-    def tojson(self):
+    def to_json(self):
         return {
             "id": self.id, 
             "username": self.username,
             "quote": self.quote,
-            "likes": self.likes,        
+            "likes": self.likes        
         }
