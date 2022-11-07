44227f94c5e77a215df8078d75eb61fd7bf9bb21
Billy Lam <billy131lam@gmail.com>
2022-11-06 19:01:57 -0800

    test quotes and anonymous

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,26 @@ import random
 
 pf = ProfanityFilter()
 
-quote_list = []
+q1 = UserQuote(1, "Joe", "The elevator to success is out of order. You'll have to use the stairs, one step at a time.")
+q1.likes = 20
+q2 = UserQuote(2, "Evan", "If you think you are too small to make a difference, try sleeping with a mosquito.")
+q2.likes = 12
+q3 = UserQuote(3, "Anonymous", "I used to think I was indecisive, but now I'm not so sure.")
+q3.likes = 1
+q4 = UserQuote(4, "Will", "The road to success is dotted with many tempting parking spaces.")
+q4.likes = 10
+q5 = UserQuote(5, "Robert", "Friendship is like peeing on yourself: everyone can see it,"
+                            "but only you get the warm feeling that it brings.")
+q5.likes = 23
+q6 = UserQuote(6, "Anonymous", "amogus")
+
+quote_list = [q1, q2, q3, q4, q5, q6]
 
 def submit_quote(username, quote):
     if pf.is_profane(quote):
         return False
+    if len(username) < 1:
+        username = "Anonymous"
     id = len(quote_list) 
     quote_object = UserQuote(id, username, quote)
     quote_list.append(quote_object)
@@ -34,5 +49,6 @@ def get_user_quote():
         weight = (quote_object.likes + 1) / (change_in_time + 1) * 100
         weights.append(weight)
     
-    print(weights)
-    return random.choices(quote_list, weights=weights, k=1)[0]
+    choice = random.choices(quote_list, weights=weights, k=1)[0]
+    choice.time = now
+    return(choice)
