06e1bddf4256d7256330a79613acc55fbb5a985e
Billy Lam <billy131lam@gmail.com>
2022-11-06 17:49:00 -0800

    back done

--- backend/UserQuote.py
+++ backend/UserQuote.py
@@ -1,14 +1,17 @@
+from datetime import datetime
+
 class UserQuote():
     def __init__(self, id, username, quote):
         self.id = id
         self.username = username
         self.quote = quote
         self.likes = 0
+        self.time = datetime.now().timestamp()
 
     def like(self):
         self.likes += 1
 
-    def tojson(self):
+    def to_json(self):
         return {
             "id": self.id, 
             "username": self.username,
--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
 from UserQuote import UserQuote
-from flask import Flask, jsonify
+import random
+from datetime import datetime
 
 quote_list = []
 
 def submit_quote(username, quote):
-    id = len(quote_list) + 1
+    id = len(quote_list)
     quote_object = UserQuote(id, username, quote)
+    now = datetime.now()
     quote_list.append(quote_object)
 
 def get_leaderboard():
     sorted_list = sorted(quote_list, key=lambda x: x.likes, reverse=True)
-    leaderboard = [quote_object for ind, quote_object in enumerate(sorted_list) if ind<5]
+    leaderboard = [quote_object for ind, quote_object in enumerate(sorted_list) if ind<10]
     leaderboard_json = [quote_object.to_json() for quote_object in leaderboard]
     return(leaderboard_json)
 
@@ -18,3 +20,23 @@ def like(id):
     for quote_object in quote_list:
         if quote_object.id == id:
             quote_object.like()
+
+def get_user_quote():
+    now = datetime.now().timestamp()
+    weights = []
+    for quote_object in quote_list:
+        change_in_time = now - quote_object.time
+        weight = (quote_object.likes + 1) / (change_in_time + 1) * 100
+        weights.append(weight)
+    
+    print(weights)
+    return(random.choices(quote_list, weights=weights, k=1)[0])
+
+submit_quote("Gordon", "gordon quote")
+submit_quote("Bob1", "quote1")
+submit_quote("Bob2", "quote2")
+submit_quote("Bob3", "quote3")
+submit_quote("Bob4", "quote4")
+
+print(quote_list)
+get_user_quote()
