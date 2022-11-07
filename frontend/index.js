dae9a0d2e26e31250b4d2942383d34739b7e77b6
gordonch <gordonch@students.cs.ubc.ca>
2022-11-06 19:48:45 -0800

    added front end

---
+++ frontend/index.js
@@ -0,0 +1,52 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+// for submit
+
+document.querySelector(".quotes").addEventListener('click', function(){
+    const quotelength = Number(length(TRIM(document.querySelector(".quotes").value)));
+    console.log(quotelength, typeof quotelength);
+}
+)
+
+let btnClear = document.querySelector('button');
+let inputs = document.querySelectorAll('input');
+btnClear.addEventListener('click', () => {
+    inputs.forEach(input =>  input.value = '');
+});
+
+
