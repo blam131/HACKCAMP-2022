c7726d63e2c79c3ed99216628c4d27414dd7417e
Billy Lam <billy131lam@gmail.com>
2022-11-06 16:10:00 -0800

    main filter

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,38 @@
-import UserQuote
+from UserQuote import UserQuote
+from flask import jsonify
+import json
 
-user1 = UserQuote(1, "gordon", "abc")
+quote_list = []
 
-user1.like()
+def submit_quote(username, quote):
+    id = len(quote_list) + 1
+    quote_object = UserQuote(id, username, quote)
+    quote_list.append(quote_object)
+
+def get_leaderboard():
+    sorted_list = sorted(quote_list, key=lambda x: x.likes, reverse=True)
+    leaderboard = [quote_object for ind, quote_object in enumerate(sorted_list) if ind<5]
+    
+    leaderboard_json = ["quotes"]
+    for quote_object in leaderboard:
+        dict = {}
+        dict["username"] = quote_object.username
+        dict["quote"] = quote_object.quote
+        dict["likes"] = quote_object.likes
+        leaderboard_json.append(dict)
+    return(json.dumps(leaderboard_json))
+
+def like(id):
+    for quote_object in quote_list:
+        if quote_object.id == id:
+            quote_object.like()
+
+submit_quote("bob", "hi")
+submit_quote("bob", "hi")
+submit_quote("bob", "hi")
+submit_quote("bob", "hi")
+submit_quote("bob", "hi")
+submit_quote("bob", "hi")
+submit_quote("bob", "hi")
+
+print(get_leaderboard())
